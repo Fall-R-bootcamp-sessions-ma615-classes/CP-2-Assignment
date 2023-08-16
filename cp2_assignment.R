f4cad3288d528ee20f1ad365fbac9fd384484452
aidanohara1 <aohara1@bu.edu>
2023-08-15 20:59:12 -0400

    init

---
+++ cp2_assignment.R
@@ -0,0 +1,78 @@
+
+# Cp-2 Assignment
+
+# Exercise 1
+
+# Create the vector (2,0,2,3) and assign it to `aVector`
+
+aVector <- 
+
+
+# Exercise 2
+
+# Create the vector (20,19,18,17,16,...,0). Call your vector `tweVector`
+
+# Create the vector (2,1.9,1.8,1.7,1.6,...,0). Call your vector `twe.Vector`
+
+# Create the vector (2,2,2,2,2,2,2,2), use the rep function. 
+  #Call your vector `twoVector`
+
+tweVector <- 
+twe.Vector <- 
+twoVector <- 
+
+
+# Exercise 3
+
+#Create a vector with the first 7 elements of pi. 
+  #Call your vector `piVector`. 
+  #After, using `rev` and `sort` create, 
+  #`pieVector` with the `piVector` elements in decreasing order 
+  #and `eipVector` with the elements in increasing order.
+
+piVector <- 
+eipVector <- 
+pieVector <- 
+
+# Exercise 4
+
+# Construct a vector called `megaVector` that contains the following elements
+  # the first and last element of `piVector` 
+  # the elements of `pieVector` equal to `3` 
+  # and the third and fourth elements of `eipVector`
+
+megaVector <- 
+
+# Exercise 5
+
+# Use logical operators in your vector selections to construct 
+  # the following from the `rivers` vector: 
+  # `noMeanRivers` :Rivers less than 200 miles and rivers more than 600 miles 
+  # `river301` :Rivers that are exactly 301 miles long 
+  # `specRivers` :Rivers between 100 and 200 miles, 
+  #        as well as rivers between 500 and 600 miles, 
+  #        do not include rivers equal to 135
+
+noMeanRivers <- 
+river301 <- 
+specRivers <- 
+
+# Exercise 6
+
+# Write a function that prints a message citing a 
+  # relativity sized river given a positive integer 
+  # representing miles, use the following rivers as breakpoints: 
+  # Feather River: 73 miles 
+  # Des Moines River: 525 miles 
+  # Rio Grande River: 1896 miles
+
+# Your function should return a string in the following format: 
+# A river X miles long is shorter than/longer than/exactly as long the "most similar river from above".
+ # ex:
+# "A river 100 miles long is longer than the Feather River."
+
+
+riverReport <- function(miles) {
+  
+}
+
